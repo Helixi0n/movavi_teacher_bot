1730b944d48bd4812e03bc99d2df3a67976be1ba
Влад <vladdomoshonkin407@gmail.com>
2024-10-28 15:27:15 +0700

    commit

--- telegrambot.py
+++ telegrambot.py
@@ -21,41 +21,65 @@ coding = ["Арина Атаманова",
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True,
+                                                 one_time_keyboard=True)
     mark_btn = telebot.types.KeyboardButton("Поставить оценку")
     rating_btn = telebot.types.KeyboardButton("Рейтинг преподавателей")
     keyboard.add(mark_btn, rating_btn)
-    bot.send_message(message.chat.id, "Привет! Поставь оценку преподу, пж", reply_markup=keyboard)
+    bot.send_message(message.chat.id,
+                     "Привет! Поставь оценку преподу, пж",
+                     reply_markup=keyboard)
 
 
 @bot.message_handler(regexp='Поставить оценку')
 def mark(message):
     keyboard = telebot.types.InlineKeyboardMarkup()
-    btn_game_graphics = telebot.types.InlineKeyboardButton("Игровая графика", callback_data="Игровая графика")
-    btn_web_design = telebot.types.InlineKeyboardButton("Дизайн сайтов", callback_data="Дизайн сайтов")
-    btn_robots = telebot.types.InlineKeyboardButton("Робототехника", callback_data="Робототехника")
-    btn_coding = telebot.types.InlineKeyboardButton("Программирование", callback_data="Программирование")
+    btn_game_graphics = telebot.types.InlineKeyboardButton("Игровая графика",
+                                                           callback_data="Игровая графика")
+    btn_web_design = telebot.types.InlineKeyboardButton("Дизайн сайтов",
+                                                        callback_data="Дизайн сайтов")
+    btn_robots = telebot.types.InlineKeyboardButton("Робототехника",
+                                                    callback_data="Робототехника")
+    btn_coding = telebot.types.InlineKeyboardButton("Программирование",
+                                                    callback_data="Программирование")
     keyboard.add(btn_game_graphics, btn_web_design, btn_robots, btn_coding)
     bot.send_message(message.chat.id, "Выбери предмет:", reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda callback: True)
-def handle_callback(callback):
+def teacher_callback(callback):
     keyboard = telebot.types.InlineKeyboardMarkup()
     if callback.data == "Игровая графика":
         for i in game_graphics:
-            keyboard.add(telebot.types.InlineKeyboardButton(i, callback_data=i))
+            keyboard.add(telebot.types.InlineKeyboardButton(i,
+                                                            callback_data=i))
     elif callback.data == "Дизайн сайтов":
         for i in web_design:
-            keyboard.add(telebot.types.InlineKeyboardButton(i, callback_data=i))
+            keyboard.add(telebot.types.InlineKeyboardButton(i,
+                                                            callback_data=i))
     elif callback.data == "Робототехника":
         for i in robots:
-            keyboard.add(telebot.types.InlineKeyboardButton(i, callback_data=i))
+            keyboard.add(telebot.types.InlineKeyboardButton(i,
+                                                            callback_data=i))
     elif callback.data == "Программирование":
         for i in coding:
-            keyboard.add(telebot.types.InlineKeyboardButton(i, callback_data=i))
+            keyboard.add(telebot.types.InlineKeyboardButton(i,
+                                                            callback_data=i))
+    bot.send_message(callback.message.chat.id,
+                     "Выбери учителя",
+                     reply_markup=keyboard)
 
-    bot.send_message(callback.message.chat.id, "Выбери учителя", reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda callback: True)
+def mark_callback(callback):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    like = telebot.types.InlineKeyboardButton("👍", callback_data="like")
+    dislike = telebot.types.InlineKeyboardButton("👎", callback_data="dislike")
+    keyboard.add(like, dislike)
+    bot.send_photo(callback.message.chat.id,
+                   f"{callback.data}.jpg",
+                   f"Преподователь: {callback.data}\n Поставьте оценку учителю",
+                   reply_markup=keyboard)
 
 
 @bot.message_handler(regexp='Рейтинг преподавателей')
